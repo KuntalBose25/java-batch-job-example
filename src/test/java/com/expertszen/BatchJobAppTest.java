ad876433b74d8b4149324806da86784652c7177d
KuntalBose25 <kuntalmtma2013@gmail.com>
2025-10-24 00:43:33 +0530

    Update BatchJobAppTest.java

--- src/test/java/com/expertszen/BatchJobAppTest.java
+++ src/test/java/com/expertszen/BatchJobAppTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 public class BatchJobAppTest {
     @Test
     public void testApp() {
-        assertTrue(true);
+        //assertTrue(true);
+        fail("Intentional failure for Jenkins email notification test");
     }
 }
